a4d407662ef2fc27d75e03ac3130391741d2eec0
Mike Wu <hello@mikewu.dev>
2023-11-08 13:59:02 +0900

    center page content

--- src/lib/ui/Page/index.tsx
+++ src/lib/ui/Page/index.tsx
@@ -5,5 +5,9 @@ interface PageProps extends PropsWithChildren {}
 
 export default function Page(props: PageProps) {
   const { children } = props
-  return <div className={styles.root}>{children}</div>
+  return (
+    <div className={styles.root}>
+      <div className={styles.content}>{children}</div>
+    </div>
+  )
 }
