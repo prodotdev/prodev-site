e136225da225a5bab88b5f9b391d91ff8868b9ea
Mike Wu <hello@mikewu.dev>
2023-12-06 06:53:31 +0900

    add svgr package, and arrow icon

--- src/lib/ui/SideNav/Section/index.tsx
+++ src/lib/ui/SideNav/Section/index.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react'
 import styles from '@/lib/ui/SideNav/Section/Section.module.css'
+import ArrowUpIcon from '@/lib/icons/arrow-up-icon.svg'
 
 interface SectionProps extends PropsWithChildren {
   title: string
@@ -12,6 +13,7 @@ export default function Section(props: SectionProps) {
     <div className={styles.root}>
       <div className={styles.header}>
         <span className={styles.title}>{title}</span>
+        <ArrowUpIcon className={styles.arrowIcon} />
       </div>
       {children}
     </div>
