8d61b7af19aa63bd66abc5f96aace637b6708503
Mike Wu <hello@mikewu.dev>
2023-12-26 09:33:46 +0900

    pre-render pages at build

--- src/app/posts/[...slug]/page.tsx
+++ src/app/posts/[...slug]/page.tsx
@@ -46,6 +46,44 @@ export default function Post(props: PostProps) {
   )
 }
 
+export async function generateStaticParams() {
+  const files = getFiles(path.join('posts'))
+
+  const paths = files.map((filename) => {
+    const slug = filename.replace('.mdx', '').split('/')
+    slug.shift() // remove initial '/posts' path
+
+    return { slug }
+  })
+
+  console.log(paths)
+
+  return paths
+}
+
+function getFiles(dirPath: string) {
+  const results: string[] = []
+
+  try {
+    const files: string[] = fs.readdirSync(dirPath)
+
+    for (const file of files) {
+      const filePath = path.join(dirPath, file)
+      const isDir = !file.includes('.mdx')
+      if (isDir) {
+        const nested = getFiles(filePath)
+        results.push(...nested)
+      } else {
+        results.push(filePath)
+      }
+    }
+  } catch (error) {
+    console.error(error)
+    //
+  }
+  return results
+}
+
 function getPost(urlPaths: string[]) {
   const postPath = urlPaths.join('/')
   const filePath = path.join('posts', postPath + '.mdx')
