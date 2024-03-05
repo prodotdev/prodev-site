2f7b8179e6ff6269094864f823eeda2f76eb8924
Mike Wu <hello@mikewu.dev>
2024-03-05 14:26:02 +0900

    blog: remove dummy post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,33 +13,7 @@ export default function Home() {
     <div className={styles.root}>
       <AppBar />
       <Page>
-        <PageMain>
-          <Title className={styles.title}>Routing</Title>
-          <PostSection>
-            <p>
-              A page is UI that is unique to a route. You can define pages by
-              exporting a component from a page.js file. Use nested folders to
-              define a route and a <strong>page.js</strong> file to make the
-              route publicly accessible.
-            </p>
-            <p>
-              Create your first page by adding a page.js file inside the app
-              directory:
-            </p>
-            <Image src={imagePlaceholder} alt="placeholder" />
-          </PostSection>
-          <div className="divider" />
-          <PostSection>
-            <Heading>Nesting Layouts</Heading>
-            <p>
-              Layouts defined inside a folder (e.g. app/dashboard/layout.js)
-              apply to specific route segments (e.g. acme.com/dashboard) and
-              render when those segments are active. By default,{' '}
-              <a href="#">layouts in the file</a> hierarchy are nested, which
-              means they wrap child layouts via their children prop.
-            </p>
-          </PostSection>
-        </PageMain>
+        <PageMain></PageMain>
       </Page>
     </div>
   )
